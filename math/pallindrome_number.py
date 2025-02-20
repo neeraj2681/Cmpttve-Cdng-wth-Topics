fad57b691f0e7674fd660ecbc91e98724143877a
neeraj <neeraj2681@gmail.com>
2025-02-20 08:39:00 +0530

    Add pallindrome number - math, basics

---
+++ math/pallindrome_number.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/palindrome-number/description/?envType=study-plan-v2&envId=top-interview-150
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        
+        reverse = 0
+
+        temp = x
+        while temp != 0:
+            reverse = reverse * 10 + temp % 10
+            temp = temp // 10
+        if x == reverse:
+            return True
+        return False
+        
